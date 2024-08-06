c7f901730de2b2015f2e1cb259ac559a17f4cddc
Eiman Asif <140498046+eiman1510@users.noreply.github.com>
2024-08-06 21:44:01 +0500

    Update Textform.js

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -24,9 +24,18 @@ export default function Textform(props) {
     const WordCount=()=>{
         const w=document.getElementById('word');
         const c=document.getElementById('count');
-        const val=text.split(w.value).length-1;
+        const val=text.split(w.value).filter((element)=>{return element.length!==0}).length;
         c.innerHTML=val+" times";
         props.showAlert("Word found "+val+" times ","Success!!!");
+    }
+    
+    const handleCopy=()=>{
+        const text1=document.getElementById("box1");
+        text1.select();
+        navigator.clipboard.writeText(text1.value);
+        document.getSelection().removeAllRanges();
+        props.showAlert("Text Copid","Success!!!");
+
     }
    
     return (
@@ -36,16 +45,17 @@ export default function Textform(props) {
             <div className="mb-3">
                 <textarea className="form-control" id="box1" rows="8" value={text} onChange={handleChange} style={{backgroundColor: props.state==='light'?'#f8f9fa':'#212529',color: props.state==='light'?'#212529':'#f8f9fa'}}></textarea>
             </div>
-            <button className={`btn btn-${props.btnClr} mx-2 my-2`} onClick={handleUp}>Convert to UpperCase</button>
-            <button className={`btn btn-${props.btnClr} mx-2 my-2`} onClick={handleLower}>Convert to LowerCase</button>
-            <button className={`btn btn-${props.btnClr} mx-2 my-2`} onClick={handleClear}>Clear</button>
+            <button disabled={text.length===0} className={`btn btn-${props.btnClr} mx-2 my-2`} onClick={handleUp}>Convert to UpperCase</button>
+            <button disabled={text.length===0} className={`btn btn-${props.btnClr} mx-2 my-2`} onClick={handleLower}>Convert to LowerCase</button>
+            <button disabled={text.length===0} className={`btn btn-${props.btnClr} mx-2 my-2`} onClick={handleClear}>Clear</button>
+            <button disabled={text.length===0} className={`btn btn-${props.btnClr} mx-2 my-2`} onClick={handleCopy}>Copy Text</button>
             </div>
             <div className="container my-3">
                 <h1>Your Text Summary</h1>
-                <p>{text.split(" ").length} words and {text.length} characters</p>
-                <p>{0.008*text.split(" ").length} minutes to read</p>
+                <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+                <p>{0.008*text.split(/\s+/).filter((element)=>{return element.length!==0}).length} minutes to read</p>
                 <h2>Preview</h2>
-                <p>{text}</p>
+                <p>{text.length>0?text:'Nothing to preview!'}</p>
                 <h2>Word Occurence</h2>
                 <input type="text" id="word" style={{backgroundColor: props.state==='light'?'#f8f9fa':'#212529',color: props.state==='light'?'#212529':'#f8f9fa'}} />
             <button className={`btn btn-${props.btnClr} mx-2 my-2`} onClick={WordCount}>Check Count</button>
